853b585971e83eebf896e57a66c76ff99d2bc3d0
Evgeny <eugen.eisner@gmail.com>
2023-03-28 19:24:14 +0200

    feat: created app

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
+from pathlib import Path
 
-if __name__ == '__main__':
-    pass
+import pdfplumber
+from art import tprint
+from gtts import gTTS
+
+
+class PdfToSpeech:
+    def main(self, file_path: Path, language: str):
+        if Path(file_path).is_file() and Path(file_path).suffix == ".pdf":
+            print(f"[+] Original file: {Path(file_path).name}")
+            print("[+] Processing...")
+
+            with pdfplumber.PDF(open(file=file_path, mode="rb")) as pdf:
+                pages = [page.extract_text() for page in pdf.pages]
+
+            text = "".join(pages)
+            text = text.replace("\n", "")
+
+            my_audio = gTTS(text=text, lang=language, slow=False)
+            file_name = Path(file_path).stem
+            my_audio.save(f"{file_name}.mp3")
+
+            return f"[+] {file_name}.mp3 saved successfully!\n---Have a good day!---"
+
+        else:
+            return "The file doesn't exist, check the file path!"
+
+    def run(self):
+        tprint("PDF>>TO>>MP3", font="bulbhead")
+        file_path = input("\nEnter a file's path: ")
+        language = input("Choose language, for example 'en', 'de' or 'ru': ")
+        if language not in ("en", "ru", "de"):
+            language = input("Choose language, for example 'en', 'de' or 'ru': ")
+        print(self.main(file_path=file_path, language=language))
+
+
+if __name__ == "__main__":
+    PdfToSpeech().run()
